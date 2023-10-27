6d64d1603f0f6787f3a238116625c780654f2a57
SSifa <stivengombo@gmail.com>
2023-10-27 11:42:32 +0300

    Created a registration activity

--- app/src/main/java/com/example/sciportal/MainActivity.java
+++ app/src/main/java/com/example/sciportal/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.sciportal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnLogin;
+    TextView tvRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Connect xml views with java objects
+        btnLogin = findViewById(R.id.btn_login);
+        tvRegister = findViewById(R.id.tv_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+                startActivity(dashboard);
+            }
+        });
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent register = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(register);
+            }
+        });
     }
 }
